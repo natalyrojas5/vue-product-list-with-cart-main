657c6d238128215e62bd1b093dca39d3741bcbd5
Nataly Rojas <natalyrojasm5@gmail.com>
2025-03-15 00:29:06 -0500

    🐛 fix: Changes

--- src/components/MyCart/MyCartEmpty.vue
+++ src/components/MyCart/MyCartEmpty.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="my-cart__empty">
-    <img src="/icons/illustration-empty-cart.svg" />
+    <img src="/icons/illustration-empty-cart.svg" alt="Icon Empty Cart" />
     <p>Your added items will appear here</p>
   </div>
 </template>
